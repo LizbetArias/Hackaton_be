68f8aa0326a788b6144fd6983a24c658b2f8d899
Lizbet Arias Tinco <127221079+LizbetArias@users.noreply.github.com>
2024-12-06 17:58:24 -0500

    Delete src directory

--- src/main/java/pe/edu/vallegrande/api_reniec/repository/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.vallegrande.api_reniec.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import pe.edu.vallegrande.api_reniec.model.Persona;
-
-public interface PersonaRepository extends ReactiveCrudRepository<Persona, Long> {
-}
